b8b5b697532ff7b12a74034a968874e4add9f8d6
mohamedamine585 <92816414+mohamedamine585@users.noreply.github.com>
2022-12-27 23:54:42 +0100

    Add files via upload

---
+++ chatUser.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'consts.dart';
+
+class chatUser {
+  final String docid;
+  final String? Username;
+  final String? email;
+  final String? photourl;
+
+  const chatUser(
+      {required this.Username,
+      required this.email,
+      required this.photourl,
+      required this.docid});
+  chatUser.fromsnapshot(QueryDocumentSnapshot<Map<String, dynamic>> snapshot)
+      : docid = snapshot.id,
+        email = snapshot.data()[chatuser_email] as String?,
+        Username = snapshot.data()[chatuser_name] as String?,
+        photourl = snapshot.data()[userphotourl] as String?;
+}
